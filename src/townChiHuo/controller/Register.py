35108274a99178581aec59cf59f27a0b84070cfa
xingxt <89220203@qq.com>
2013-03-11 22:25:48 +0800

    'register'

---
+++ src/townChiHuo/controller/Register.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#login controller actions
+
+
+from settings import settings
+from mako_render import mako_render
+
+import web
+
+class login:
+    def GET(self, *path):
+        web.header('Content-Type', 'text/html')
+        return mako_render('register/register.tmpl')
+    
+
+
+
