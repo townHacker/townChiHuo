c94af06aa708d17f3de9c7b9e9bc5cd04a4b0411
zhouyunchang <zhou.yunchang@hotmail.com>
2013-03-13 17:33:41 +0800

    修改model.py, user.py, mongodb_hack.py, 添加util目录, 用于存放一个工具模块

--- src/townChiHuo/models/model.py
+++ src/townChiHuo/models/model.py
@@ -3,13 +3,55 @@
 
 __metaclass__ = type
 
+def model(doc):
+    '''
+    将一个document(mongoDB中的Document)初始为对象
+    '''
+    return Model(doc=doc)
+
+
+def query(collection, **kwargs):
+    '''
+    查询
+    '''
+    docs = collection.find(kwargs)
+    for doc in docs:
+        yield model(doc)
+
+
+def insert(collection, model):
+    '''
+    插入
+    '''
+    return collection.insert(model.__dict__['_doc'])
+
+def remove(collection, model):
+    '''
+    删除
+    '''
+    doc = collection.remove(model.__dict__['_doc'])
+    m_fun = global model
+    if doc:
+        doc = m_fun(doc)
+    return doc
+
+
+def save(collection, model):
+    '''
+    若存在_id则更新, 否则添加
+    '''
+    return collection.save(model.__dict__['_doc'])
+    
+
 class Model:
     '''
     模型基类，所有的模型类都应该继承该类
     '''
-    def __init__(self):
+    def __init__(self, doc=None):
         # 单下划线开关的成员变量为保护变量，只有类及子类可以访问
-        self.__dict__["_doc"] = {}
+        if doc is None:
+            doc = {}
+        self.__dict__["_doc"] = doc
 
     
     def __setattr__(self, name, value):
@@ -23,6 +65,7 @@ class Model:
         '''
         获取属性
         '''
+        # 如果字典中不存在键name, 默认返回None
         return self.__dict__["_doc"].get(name)
         
 
@@ -38,8 +81,12 @@ class Model:
 
 
 if __name__ == '__main__':
-    model = Model()
-    print model.__dict__
-    model.a = 'abc'
-    print model._doc['a']
-    print model._doc
+    m = Model()
+    print m.__dict__
+    m.a = 'abc'
+    print m._doc['a']
+    print m._doc
+    d = {'name': 'zhouyunchang'}
+    global model
+    user = model(d)
+    print user.name
--- src/townChiHuo/models/user.py
+++ src/townChiHuo/models/user.py
@@ -8,78 +8,72 @@ import datetime
 import time
 from hashlib import md5
 
-from mongodb_hack import mongodb_hack
-import settings
+from townChiHuo.mongodb_hack import mongodb_hack as db_hack
+from townChiHuo.models import model
 
-class User:
+class User(model.Model):
     '''
     用户
-    '''
     user_id = None
-    name = None # 用户名
-    email = None # 邮箱
-    password = None # 密码 
-    real_name = None # 真实姓名
-    sex = None # 性别
+    user_name # 用户名
+    email # 邮箱
+    password # 密码
+    last_name # 姓
+    first_name # 名
+    sex # 性别
 
-    role = [] # 用户角色
-    login_info = None # 用户登录信息
+    role # 用户角色
+    login_info # 用户登录信息
+    '''
 
-    def __init__(self, name=None, email=None):
-        self.name = name;
-        self.email = email;
-        self.user_id = uuid.uuid4()
-
-    @staticmethod
-    def login(u_name, password, timestamp=None, record=False):
-        '''
-        用户登录
-        u_name: 用户名
-        password: 密码
-        timestamp: 登录时间戳
-        record: 是否需要记录登录时间戳
-        '''
-        users = MongoHack(collection='users').collection
-        user = users.find_one( \
-            {'name': u_name, \
-                'password': password})
-        if user is None:
-            # 用户名密码不正确或用户不存在
-            return False
+def login(u_name, password, timestamp=None, record=False):
+    '''
+    用户登录
+    u_name: 用户名
+    password: 密码
+    timestamp: 登录时间戳
+    record: 是否需要记录登录时间戳
+    '''
+    users = db_hack.connect(collection='users')
+    user = users.find_one( \
+        {'name': u_name, \
+             'password': password})
+    if user is None:
+        # 用户名密码不正确或用户不存在
+        return False
+    else:
+        # 验证登录时间戳
+        is_succeed = False
+        if user.login_info.login_timestamp is not None:
+            is_succeed = user.login_info.login_timestamp == timestamp
         else:
-            # 验证登录时间戳
-            is_succeed = False
-            if user.login_info.login_timestamp is not None:
-                is_succeed = user.login_info.login_timestamp == timestamp
-            else:
-                is_succeed = True
-                
+            is_succeed = True
+            
             if is_succeed:
                 update_login_info(user, record)
                 users.save(user)
             return is_succeed
 
-    
-    @staticmethod
-    def register(email, password):
-        '''
-        用户注册
-        email: 邮箱
-        password: 密码
-        '''
-                
-        user = User()
-        user.email, user.name = email, email
-        user.password = md5(password)
-        users = MongoHack(collection='users').collection
-
-        if None is not \
-            users.find_one({'email': email}):
-            raise Exception('该邮箱已经注册过，请使用其它邮箱注册.')
-        else:
-            objectId = users.insert(user)
-            return users.find({'_id': objectId})
         
+def register(email, password):
+    '''
+    用户注册
+    email: 邮箱
+    password: 密码
+    '''
+    
+    user = User()
+    user.email, user.name = email, email
+    user.password = md5(password)
+    users = db_hack.connect(collection='users')
+
+    if None is not \
+            users.find_one({'email': email}):
+        raise Exception('该邮箱已经注册过，请使用其它邮箱注册.')
+    else:
+        objectId = users.insert(user)
+        return users.find({'_id': objectId})
+    
             
         
 def md5(password):
@@ -105,10 +99,10 @@ def update_login_info(user, record=False):
 class LoginInfo:
     '''
     登录信息
+    last_login_dt  # 最后一次登录时间
+    login_timestamp  # 登录时间戳
     '''
-    last_login_dt = None # 最后一次登录时间
-    login_timestamp = None # 登录时间戳
-
+    
     
 if __name__ == '__main__':
-    User.register('admin', '123456')
+    register('admin', '123456')
