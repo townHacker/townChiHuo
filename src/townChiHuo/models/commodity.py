a3aa62eda0fb434630591f00fffbbb560e562e20
zhouyunchang <zhou.yunchang@hotmail.com>
2013-04-17 17:48:06 +0800

    添加models/commodity.py商品模型类文件

---
+++ src/townChiHuo/models/commodity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from townChiHuo.models.model import Model
+
+class Commodity(Model):
+    '''
+    商品
+    commodity_id: 商品id
+    name: 商品名称
+    brief_name: 简短名称
+    description: 商品描述
+    '''
+    pass
