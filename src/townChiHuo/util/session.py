cd74ee16a6d40d84c24b6a4fab206d3d3367d0ae
Jeffrey Zhou <jeffrey@bamboo.com>
2013-05-07 21:33:27 +0800

    modify session.py to be compatible with pylibmc

--- src/townChiHuo/util/session.py
+++ src/townChiHuo/util/session.py
@@ -5,6 +5,11 @@ import time
 import web
 from web.session import Store
 
+try:
+    import pylibmc as memcache
+except:
+    import memcache
+
 __all__ = [
     'MemcachedStore', 
     ]
@@ -14,29 +19,55 @@ class MemcachedStore(Store):
     session be stored in memcached
     inherit web.session.Store(see web.py web framework session.py source file)
     '''
+    
     def __init__(self, mc):
-        self._mc = mc;
+        if hasattr(memcache, 'ClientPool'):
+            self.pool = memcache.ClientPool(mc, 4)
+        else:
+            self._mc = mc;
+
+    def _get(self, key):
+        if hasattr(self, 'pool'):
+            with self.pool.reserve() as mc:
+                return mc.get(key)
+        else:
+            return self._mc.get(key)
+
+    def _set(self, *args):
+        if hasattr(self, 'pool'):
+            with self.pool.reserve() as mc:
+                mc.set(*args)
+        else:
+            self._mc.set(*args)
+
+    def _delete(self, key):
+        if hasattr(self, 'pool'):
+            with self.pool.reserve() as mc:
+                mc.delete(key)
+        else:
+            self._mc.delete(key)
+    
     
     def __contains__(self, key):
-        return True if self._mc.get(key) else False
+        return True if self._get(key) else False
 
     def __getitem__(self, key):
-        print 'get_session(memcache_key): ', key        
-        value = self._mc.get(key)
+        print 'get_session(memcache_key): ', key
+        value = self._get(key)
         if value:
             value['atime'] = time.time()
-            self._mc.set(key, value)
+            self._set(key, value)
         return value
 
     def __setitem__(self, key, value):
         print 'set_session(memcache_key): ', key
         now = time.time()
         value['atime'] = now
-        self._mc.set(key, value, web.config.session_parameters['timeout'])
+        self._set(key, value, web.config.session_parameters['timeout'])
         
     def __delitem__(self, key):
         print 'del_session(memcache_key): ', key
-        self._mc.delete(key)
+        self._delete(key)
     
 
     def cleanup(self, timeout):
